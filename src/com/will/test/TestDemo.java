59ba2abf2ba68076ac8f2426db10beda4e363a73
wgh <willwu618@gmail.com>
2017-11-23 14:14:49 +0800

    1--2017/11/23

--- src/com/will/test/TestDemo.java
+++ src/com/will/test/TestDemo.java
@@ -1,10 +1,24 @@
 package com.will.test;
 
+
+import com.will.utils.SortUtil;
+
 public class TestDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] param = {49,38,65,97,76,13,27,49,78,34,12,64,1,8};
+		int[] result = SortUtil.sort(param);
+		
+		for (int i = 0;i < result.length;i ++) {
+			System.out.print(result[i] + " ");
+		}
+		System.out.println();
+		System.out.println("**********************************");
+		result = SortUtil.sortBySelect(param);
+		
+		for (int i = 0;i < result.length;i ++) {
+			System.out.print(result[i] + " ");
+		}
 	}
 
 }
--- src/com/will/utils/SortUtil.java
+++ src/com/will/utils/SortUtil.java
@@ -1,5 +1,46 @@
 package com.will.utils;
 
 public class SortUtil {
-
+	
+	/**
+	 * 自己写的排序--从小到大
+	 * 找到比min还小的就和第一个数（未排好的）的交换
+	 * @param param
+	 * @return
+	 */
+	public static int[] sort(int[] param) {
+		for (int i = 0;i < param.length;i ++) {
+			int min = param[i];
+			for (int j = i;j < param.length - 1;j ++) {
+				if (min > param[j+1]) {
+					min = param[j+1];
+					param[j+1] = param[i];
+					param[i] = min;
+				}
+			}
+		}
+		return param;
+	}
+	
+	/**
+	 * 选择排序--从小到大
+	 * 每一遍找到最小的和第一个数（未排好的）交换
+	 * @param param
+	 * @return
+	 */
+	public static int[] sortBySelect(int[] param) {
+		for (int i = 0;i < param.length;i ++) {
+			int min = param[i];
+			int index = i;
+			for (int j = i + 1;j < param.length;j ++) {
+				if (min > param[j]) {
+					min = param[j];
+					index = j;
+				}
+			}
+			param[index] = param[i];
+			param[i] = min;
+		}
+		return param;
+	}
 }
